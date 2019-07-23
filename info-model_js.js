b5bd707d8e14a0f7d31ff93e983470aee76b5687
Omar Elnagdy <Omar.Elnagdy@ibm.com@omars-mbp.armonk.ibm.com>
2019-07-23 08:07:27 -0400

    everything

---
+++ info-model_js.js
@@ -0,0 +1,64 @@
+<div className="table">
+                                <div className="cool-facts">
+                                    <hr />
+                                    <span className="cool-facts-title">Cool Facts</span><img className="creative-icons" src={creative} alt="creative" />
+                                    <hr />
+                                    <div className="facts">
+                                        <ul>
+                                            <li style={{lineHeight: '1.8'}}>
+                                                - Southbury IBM is the largest corporate complex in Southbury
+                                            </li>
+                                            <li style={{lineHeight: '1.8'}}>
+                                                - Remade its old cafeteria into a new agile space which opened the summer of 2018
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="events">
+                                    <hr />
+                                    <span className="events-title">Weekly Events</span><img className="weekly-icons" src={calendar} alt="calendar" />
+                                    <hr />
+                                    <ul className="weekly-events">
+                                        <li>Yoga Mondays <img className="weekly-icons" src={yoga} alt="ping" /></li>
+                                        <li>BBQ Tuesdays <img className="weekly-icons" src={bbq} alt="ping" /></li>
+                                        <li>Soccer Wednesdays <img className="weekly-icons" src={soccer} alt="ping" /></li>
+                                        <li>Ping Pong Thursdays <img className="weekly-icons" src={ping} alt="ping" /></li>
+                                        <li>Movie Fridays <img className="weekly-icons" src={movie} alt="ping" /></li>
+                                    </ul>
+                                </div>
+                                <div className="trips">
+                                    <hr />
+                                    <span className="trips-title">IBM Trips</span><img className="vacation-icons" src={vacation} alt="vacation" />
+                                    <hr />
+                                    <iframe 
+                                        src="https://snapwidget.com/embed/716192"   
+                                        class="snapwidget-widget" 
+                                        allowtransparency="true" 
+                                        frameborder="0" 
+                                        scrolling="no" 
+                                        className="ig-slideshow">
+                                    </iframe>
+                                </div>
+                                
+                                <div>
+                                    <a 
+                                        href="https://twitter.com/intent/tweet?button_hashtag=ibmatsouthbury&ref_src=twsrc%5Etfw" 
+                                        target="_blank"
+                                        class="twitter-hashtag-button" 
+                                        data-show-count="false">Tweet #ibmatsouthbury
+                                    </a>
+                                    <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+                                </div>
+                                <div className="social-media">
+                                    <h1 className="social-media-title">Follow us @Social Media</h1>
+                                    <div className="icon-row">
+                                        <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/lifeatibmsouthbury/">
+                                            <img className="icons"  src={instagram} alt="ig"/>
+                                        </a>
+                                        <img className="icons" src={snapcode} alt="snapcode" />
+                                        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ibmatsouthbury">
+                                            <img className="icons"  src={twitter} alt="twitter"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
---
+++ locations.js
@@ -0,0 +1,80 @@
+const newLocal = {
+    type: 'FeatureCollection',
+    features: [{
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-73.721237, 41.107543]
+        },
+        properties: {
+            phoneFormatted: '(XXX) XXX-XXXX',
+            phone: 'XXXXXXXXXX',
+            address: '1 Orchard Rd',
+            city: 'Armonk',
+            country: 'United States',
+            state: 'New York'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-73.972903, 40.762247]
+        },
+        properties: {
+            phoneFormatted: '(XXX) XXX-XXXX',
+            phone: 'XXXXXXXXXX',
+            address: '590 Madison Avenue',
+            city: 'New York City',
+            country: 'United States',
+            state: 'New York'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-73.990318, 40.730188]
+        },
+        properties: {
+            phoneFormatted: '(XXX) XXX-XXXX',
+            phone: 'XXXXXXXXXX',
+            address: '51 Astor Place',
+            city: 'New York City',
+            country: 'United States',
+            state: 'New York'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-73.985037, 40.743631]
+        },
+        properties: {
+            phoneFormatted: '(XXX) XXX-XXXX',
+            phone: 'XXXXXXXXXX',
+            address: '63 Madison Ave',
+            city: 'New York City',
+            country: 'United States',
+            state: 'New York'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-73.211491, 41.469375]
+        },
+        properties: {
+            phoneFormatted: '(XXX) XXX-XXXX',
+            phone: 'XXXXXXXXXX',
+            address: '150 Kettletown Rd',
+            city: 'Southbury',
+            country: 'United States',
+            state: 'Connecticut'
+        }
+    }
+    ]
+};
+export { newLocal };
